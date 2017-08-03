95c96ba6479a558b2c0e2cd6e70efc0b1971f82c
Kiem Nguyen <kiemnguyen@gmail.com>
2017-08-03 02:32:28 -0500

    Added auto complete

---
+++ coding_challenges/auto_complete.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 11 16:56:22 2018
+
+@author: kiem
+ASENDIN
+ASPERGEL
+ASPERTAME
+ASPIRIN
+ATAVAN
+BUPROPTAN
+SABRIL
+SALEX
+"""
+
+def auto_completed()
+
