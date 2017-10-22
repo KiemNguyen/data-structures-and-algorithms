c991259b8e4e0f7cbcfea04b07ed18b8cf96d1cf
Kiem Nguyen <kiemnguyen@gmail.com>
2017-10-22 17:57:36 -0500

    Refactored

--- data_structures/check_brackets.py
+++ data_structures/check_brackets.py
@@ -17,24 +17,23 @@ class Bracket:
         return False
 
 if __name__ == "__main__":
-    #text = sys.stdin.read()
-    text = '{[}'
+    text = sys.stdin.read()
     opening_brackets_stack = []
     for i, next in enumerate(text):
         if next == '(' or next == '[' or next == '{':
             # Process opening bracket, write your code here
             opening_brackets_stack.append(Bracket(next, i))
-            
+
         if next == ')' or next == ']' or next == '}':
             # Process closing bracket, write your code here
             if len(opening_brackets_stack) == 0:
                 opening_brackets_stack.append(Bracket(next, i))
-            
+
             top = opening_brackets_stack.pop()
-            
+
             if not top.Match(next):
                 opening_brackets_stack.append(Bracket(next, i))
-            
+
     # Printing answer, write your code here
     if len(opening_brackets_stack) == 0:
         print("Success")
