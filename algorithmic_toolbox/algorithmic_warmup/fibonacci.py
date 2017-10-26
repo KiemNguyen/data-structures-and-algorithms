e04f24071076e0d9dd2d5517caea12f4dd106c07
Kiem Nguyen <kiemnguyen@gmail.com>
2017-10-26 16:49:50 -0500

    Added huge fibonacci

--- algorithmic_toolbox/algorithmic_warmup/fibonacci.py
+++ algorithmic_toolbox/algorithmic_warmup/fibonacci.py
@@ -7,8 +7,9 @@ def calc_fib(n):
     return calc_fib(n - 1) + calc_fib(n - 2)
 '''
 
+
 def calc_fib(n):
-    if (n <= 1):
+    if n <= 1:
         return n
     f = []
     # 0th and 1st number are 0 and 1
@@ -16,8 +17,9 @@ def calc_fib(n):
     f.append(1)
 
     for i in range(2, 46):
-        f.append(f[i-1] + f[i-2])
+        f.append(f[i - 1] + f[i - 2])
     return f[n]
-    
+
+
 n = int(input())
 print(calc_fib(n))
